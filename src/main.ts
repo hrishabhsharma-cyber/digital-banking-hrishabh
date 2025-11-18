ec3e6fa4b4541d5b21a12f96af630941837d417a
Hrishabh Sharma <hrishabh.sharma@ambak.com>
2025-11-18 12:34:05 +0530

    third commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   console.log('Server is running on port', process.env.PORT ?? 3000);
-  console.log('Server is running on port', process.env.PORT ?? 3000);
 }
 void bootstrap();
