383e30db4acfb8ab3e30309f34a8767f48dbebaf
Hrishabh Sharma <hrishabh.sharma@ambak.com>
2025-11-29 16:45:38 +0530

    remove hard code port number

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(Number(process.env.PORT || 3000));
+  await app.listen(Number(process.env.PORT));
   console.log('Server is running on port 3000');
 }
 void bootstrap();
